2e85b35777b606a0b05ed3d67451240fa0403577
CharlesBoury <boury.charles@gmail.com>
2017-03-15 00:32:04 +0100

    Etape 2: camera follow

---
+++ Assets/Scripts/Follow.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Follow : MonoBehaviour {
+
+	public Transform cible;
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+		Camera c = GetComponent<Camera>();
+
+		if (cible.localPosition.y - transform.localPosition.y >= c.orthographicSize) {
+			transform.localPosition = new Vector3(
+				transform.localPosition.x,
+				transform.localPosition.y + c.orthographicSize * 2,
+				transform.localPosition.z);
+		}
+		if (cible.localPosition.y - transform.localPosition.y <= -c.orthographicSize) {
+			transform.localPosition = new Vector3(
+				transform.localPosition.x,
+				transform.localPosition.y - c.orthographicSize * 2,
+				transform.localPosition.z);
+		}
+		if (cible.localPosition.x - transform.localPosition.x >= c.orthographicSize * c.aspect) {
+			transform.localPosition = new Vector3(
+				transform.localPosition.x + c.orthographicSize * c.aspect * 2,
+				transform.localPosition.y,
+				transform.localPosition.z);
+		}
+		if (cible.localPosition.x - transform.localPosition.x <= -c.orthographicSize * c.aspect) {
+			Debug.Log("Blarp");
+			transform.localPosition = new Vector3(
+				transform.localPosition.x - c.orthographicSize * c.aspect * 2,
+				transform.localPosition.y,
+				transform.localPosition.z);
+		}
+	}
+}
