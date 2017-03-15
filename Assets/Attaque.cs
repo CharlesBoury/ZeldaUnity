2d0bbd849e3850bca4982d5e22497934a39697d7
CharlesBoury <boury.charles@gmail.com>
2017-03-15 23:00:07 +0100

    Etape 5: atk et tuer les méchants

---
+++ Assets/Attaque.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attaque : MonoBehaviour {
+
+	public GameObject epee;
+	
+	void Update () {
+		if (Input.GetKeyDown("space")) {
+			epee.SetActive(true);
+		} else {
+			epee.SetActive(false);
+		}
+	}
+}
---
+++ Assets/Scripts/InfligeDegats.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InfligeDegats : MonoBehaviour {
+
+	void OnTriggerEnter2D(Collider2D coll) {
+		if (coll.gameObject.tag == "Enemy") {
+			Destroy (coll.gameObject);
+		}
+	}
+}
