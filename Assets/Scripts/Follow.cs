cb79cd6d9fd54985a752edc5013ec678e3e07207
CharlesBoury <boury.charles@gmail.com>
2017-03-16 00:44:10 +0100

    cameraFollow: ne se plaint plus s'il n'y a rien a suivre

--- Assets/Scripts/Follow.cs
+++ Assets/Scripts/Follow.cs
@@ -5,38 +5,37 @@ using UnityEngine;
 public class Follow : MonoBehaviour {
 
 	public Transform cible;
-
-	void Start () {
-		
-	}
 	
 	void Update () {
 		Camera c = GetComponent<Camera>();
 
-		if (cible.localPosition.y - transform.localPosition.y >= c.orthographicSize) {
-			transform.localPosition = new Vector3(
-				transform.localPosition.x,
-				transform.localPosition.y + c.orthographicSize * 2,
-				transform.localPosition.z);
+		// s'il y a une cible
+		if (cible != null) {
+			if (cible.localPosition.y - transform.localPosition.y >= c.orthographicSize) {
+				transform.localPosition = new Vector3(
+					transform.localPosition.x,
+					transform.localPosition.y + c.orthographicSize * 2,
+					transform.localPosition.z);
+			}
+			if (cible.localPosition.y - transform.localPosition.y <= -c.orthographicSize) {
+				transform.localPosition = new Vector3(
+					transform.localPosition.x,
+					transform.localPosition.y - c.orthographicSize * 2,
+					transform.localPosition.z);
+			}
+			if (cible.localPosition.x - transform.localPosition.x >= c.orthographicSize * c.aspect) {
+				transform.localPosition = new Vector3(
+					transform.localPosition.x + c.orthographicSize * c.aspect * 2,
+					transform.localPosition.y,
+					transform.localPosition.z);
+			}
+			if (cible.localPosition.x - transform.localPosition.x <= -c.orthographicSize * c.aspect) {
+				transform.localPosition = new Vector3(
+					transform.localPosition.x - c.orthographicSize * c.aspect * 2,
+					transform.localPosition.y,
+					transform.localPosition.z);
+			}
 		}
-		if (cible.localPosition.y - transform.localPosition.y <= -c.orthographicSize) {
-			transform.localPosition = new Vector3(
-				transform.localPosition.x,
-				transform.localPosition.y - c.orthographicSize * 2,
-				transform.localPosition.z);
-		}
-		if (cible.localPosition.x - transform.localPosition.x >= c.orthographicSize * c.aspect) {
-			transform.localPosition = new Vector3(
-				transform.localPosition.x + c.orthographicSize * c.aspect * 2,
-				transform.localPosition.y,
-				transform.localPosition.z);
-		}
-		if (cible.localPosition.x - transform.localPosition.x <= -c.orthographicSize * c.aspect) {
-			Debug.Log("Blarp");
-			transform.localPosition = new Vector3(
-				transform.localPosition.x - c.orthographicSize * c.aspect * 2,
-				transform.localPosition.y,
-				transform.localPosition.z);
-		}
+
 	}
 }
