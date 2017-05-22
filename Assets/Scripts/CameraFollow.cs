522a7209b090ec194c49f897d6c5dd88bff37ee4
CharlesBoury <boury.charles@gmail.com>
2017-05-22 09:43:16 +0200

    Camera smooth & monstres activés par écran

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+	public Transform cible;
+
+	public Camera cam;
+	float cameraL;
+	float cameraH;
+
+	public int gridX = 0;
+	public int gridY = 0;
+
+	public Animator animator;
+
+	void Start(){
+		cameraL = cam.orthographicSize * 2 * cam.aspect;
+		cameraH = cam.orthographicSize * 2;
+	}
+	
+	void Update () {
+		// s'il y a une cible
+		if (cible != null) {
+			gridX = Mathf.FloorToInt(cible.localPosition.x/cameraL);
+			gridY = Mathf.FloorToInt(cible.localPosition.y/cameraH);
+		}
+
+		animator.SetInteger("X", gridX); 
+		animator.SetInteger("Y", gridY); 
+	}
+}
---
+++ Assets/Scripts/ScreenManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenManager : MonoBehaviour {
+
+	// active/desactive les gameobjects propres à chaque ecran.
+
+	public List<GameObject> screensX0;
+	public List<GameObject> screensX1;
+
+	public CameraFollow cam;
+
+	void Update () {
+		if (cam != null)
+		{
+			for(int i = 0; i < 2; i++)
+			{
+				for(int j = 0; j < 2; j++)
+				{
+					bool isActive = cam.gridX == i && cam.gridY == j;
+					if ( i == 0 ) screensX0[j].SetActive(isActive);
+					else screensX1[j].SetActive(isActive);
+				}
+			}
+		}
+	}
+}
