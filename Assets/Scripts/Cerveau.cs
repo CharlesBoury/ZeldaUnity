d1057afdcee342221783d47a8fd02f69a8fc22d1
CharlesBoury <boury.charles@gmail.com>
2017-05-24 17:57:30 +0200

    Les monstres commencent tout de suite à bouger

--- Assets/Scripts/Cerveau.cs
+++ Assets/Scripts/Cerveau.cs
@@ -13,10 +13,20 @@ public class Cerveau : MonoBehaviour
 		ResetDirections();
 	}
 
-	public void ResetDirections() {
+	public void ResetDirections()
+	{
 		up    = false;
 		down  = false;
 		left  = false;
 		right = false;
+	}
+
+	public void DirectionAleatoire()
+	{
+		int n = Random.Range(0,4);
+		up    = (n==0);
+		down  = (n==1);
+		left  = (n==2);
+		right = (n==3);
 	}
 }
