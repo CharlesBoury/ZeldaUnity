92b895d1d0d67742968c1e05f11b44406087b2c6
CharlesBoury <boury.charles@gmail.com>
2017-07-10 03:00:38 +0200

    Display coeurs ! + Assombrir joueur sous les portes

--- Assets/Scripts/AssombrirJoueur.cs
+++ Assets/Scripts/AssombrirJoueur.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(BoxCollider2D))]
-public class Grotte : MonoBehaviour {
+public class AssombrirJoueur : MonoBehaviour {
 
 	public AnimationCurve ease;
 	BoxCollider2D myBox;
@@ -19,10 +19,14 @@ public class Grotte : MonoBehaviour {
 			SpriteRenderer sprite = collider2D.gameObject.GetComponent<SpriteRenderer>();
 			if (sprite != null)
 			{
-				float dist = myBox.bounds.max.y - collider2D.transform.position.y;
+				// percent : de combien est rentré le joueur dans le trigger PAR LE BAS
+				float dist = collider2D.transform.position.y - myBox.bounds.min.y;
 				float percent = dist/myBox.bounds.size.y;
+
+				// on transforme ce pourcentage en couleur : 0% = blanc, 100% = noir
+				// en prenant en compte la courbe (0 = blanc / 1 = noir)
 				percent = ease.Evaluate(percent);
-					sprite.color = new Color(percent, percent, percent, 1F);
+				sprite.color = new Color(1-percent, 1-percent, 1-percent, 1F);
 			}
 		}
 	}
---
+++ Assets/Scripts/UI/DisplayCoeurs.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[ExecuteInEditMode]
+public class DisplayCoeurs : MonoBehaviour {
+
+	public Vie vie;
+	public Sprite coeurVide;
+	public Sprite coeurMoitie;
+	public Sprite coeurPlein;
+
+	public GameObject coeurPrefab;
+
+	int currentPvMax = 0;
+
+	void Update()
+	{
+		if (currentPvMax != vie.pvMax)
+		{
+			modifierNombreDeCoeurs();
+			currentPvMax = vie.pvMax;
+		}
+
+		for(int i = 1; i <= vie.pvMax; i += 2) // 1,3,5...
+		{
+			if (transform.childCount > (i-1)/2) // sinon error quand pv > pvMax
+			{
+				if (vie.pv <  i) transform.GetChild((i-1)/2).GetComponent<Image>().sprite = coeurVide;
+				if (vie.pv == i) transform.GetChild((i-1)/2).GetComponent<Image>().sprite = coeurMoitie;
+				if (vie.pv >  i) transform.GetChild((i-1)/2).GetComponent<Image>().sprite = coeurPlein;
+			}
+		}
+	}
+
+	void modifierNombreDeCoeurs()
+	{
+		int coeursNecessaires = (int)Mathf.Ceil(vie.pvMax/2.0F);
+		// Debug.Log("----------------------");
+		// Debug.Log(coeursNecessaires + " coeursNecessaires.");
+		if (coeursNecessaires < transform.childCount)
+		{
+			for(int i = transform.childCount - 1; i > coeursNecessaires - 1; i --)
+			{
+				// Debug.Log("destroy Child "+i);
+				GameObject.DestroyImmediate(transform.GetChild(i).gameObject);
+			}
+		}
+		else if (coeursNecessaires > transform.childCount)
+		{
+			for(int i = transform.childCount ; i < coeursNecessaires; i ++)
+			{
+				// Debug.Log("Création du coeur numéro "+i);
+				Instantiate(coeurPrefab, transform);
+			}
+		}
+	}
+}
